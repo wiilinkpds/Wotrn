6620a8871e510d58f7346e3dec006e1406f2771b
Nicolas N <nicolas.naudot@epita.fr>
2012-12-05 14:20:29 +0100

    La base du commencement

---
+++ GameProject/GameProject/GameProject/MainGame.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using GameProject.Menus;
+using GameProject.Managers;
+
+namespace GameProject
+{
+    public class MainGame : Microsoft.Xna.Framework.Game
+    {
+        static public GraphicsDeviceManager graphics;
+        static public SpriteBatch SpriteBatch;
+
+        static public TexturesManager TexturesMenu { get; private set; }
+
+        public const int ScreenX = 1024;
+        public const int ScreenY = 768;
+
+        public MainGame()
+        {
+            graphics = new GraphicsDeviceManager(this) { PreferredBackBufferWidth = ScreenX, PreferredBackBufferHeight = ScreenY };
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            SpriteBatch = new SpriteBatch(GraphicsDevice);
+            TexturesMenu = new TexturesManager();
+            TexturesMenu.LoadMenu(Content);
+        }
+
+        protected override void UnloadContent()
+        {
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            //if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+            //    this.Exit();
+
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+            SpriteBatch.Begin();
+
+
+            MainMenu.mainMenu();
+
+
+            SpriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
