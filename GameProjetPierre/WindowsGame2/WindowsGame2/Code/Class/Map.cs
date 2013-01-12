b87847c5ebeddda62575ef43debbd5ebb579adc5
Wiilinkpds <wiilink@live.fr>
2013-01-12 14:52:48 +0100

    Camera bloquant juste sur le bord gauche

---
+++ GameProjetPierre/WindowsGame2/WindowsGame2/Code/Class/Map.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using System.IO;
+
+namespace Jeux
+{
+    class Map
+    {
+        Tile[,] tiles;
+        Vector2 size;
+
+        public Tile[,] Tiles
+        {
+            get { return tiles; }
+            set { tiles = value; }
+        }
+
+        public Vector2 Size
+        {
+            get { return size; }
+        }
+
+        public Vector2 PixelSize
+        {
+            get { return size * tiles[0, 0].Size; }
+        }
+
+        public Map()
+        {
+        }
+
+        public Map(Vector2 size)
+        {
+            this.size = size;
+            tiles = new Tile[(int)size.X, (int)size.Y];
+        }
+
+        public void DefaultLoadContent(TextureManager textureManager, string defaulTextureName)
+        {
+            for (int i = 0; i < Size.X; i++)
+            {
+                for (int j = 0; j < Size.Y; j++)
+                {
+                    Tiles[i, j] = new Tile(new Vector2(i, j));
+                    tiles[i, j].LoadContent(textureManager, defaulTextureName);
+                }
+            }
+        }
+        /*
+        public void LoadFileContent(string fileName, ref Map map)
+        {
+            StreamReader SR;
+            string line = "";
+            int j = 0;
+            try
+            {
+                SR = new StreamReader(fileName);
+            }
+            catch (Exception)
+            {
+
+            }
+            while ((line = SR.ReadLine()) != null && j < map.tiles.GetLength(1))
+            {
+                if (line.Length < map.tiles.GetLength(0))
+                {
+                    SR.Close();
+                }
+            }
+        }
+        */
+        public void LoadContent(TextureManager textureManager)
+        {
+            for (int i = 0; i < tiles.GetLength(0); i++)
+            {
+                for (int j = 0; j < tiles.GetLength(1); j++)
+                {
+                    tiles[i, j].LoadContent(textureManager);
+                }
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            for (int i = 0; i < tiles.GetLength(0); i++)
+            {
+                for (int j = 0; j < tiles.GetLength(1); j++)
+                {
+                    tiles[i, j].Draw(spriteBatch);
+                }
+            }
+        }
+    }
+}
---
+++ GameProjetPierre/WindowsGame2/WindowsGame2/Code/Class/Sprite.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Jeux
+{
+    class Sprite
+    {
+        Texture2D texture;
+        Vector2 position;
+        Vector2 origin = Vector2.Zero;
+        Vector2 scale = Vector2.One;
+        Rectangle destinationRectangle;
+        Rectangle? sourceRectangle = null;
+        Rectangle? colisionRectangle = null;
+        Color color;
+        SpriteEffects effect = SpriteEffects.None;
+        bool colisionRectangleVisible = false;
+        bool colision = false;
+        Texture2D textureColisionRectangle;
+        float indexX = 0;
+        float indexY = 0;
+        float rotation = 0;
+        float layerDepth = 0;
+        int maxIndexX = 0;
+        int maxIndexY = 0;
+        string readingDimension;
+
+        public Vector2 Position
+        {
+            get { return position; }
+            set { position = value; }
+        }
+
+        public Rectangle DestinationRectangle
+        {
+            get
+            {
+                if (destinationRectangle == null)
+                {
+                    this.destinationRectangle = new Rectangle((int)position.X, (int)position.Y, SourceRectangle.Value.Width, SourceRectangle.Value.Height);
+                    return (Rectangle)destinationRectangle;
+                }
+                else
+                    return (Rectangle)destinationRectangle;
+            }
+            set { this.destinationRectangle = value; }
+        }
+
+        public Rectangle? SourceRectangle
+        {
+            get
+            {
+                if (sourceRectangle == null)
+                {
+                    this.destinationRectangle = new Rectangle(0, 0, texture.Width, texture.Height);
+                    return (Rectangle)sourceRectangle;
+                }
+                else
+                    return (Rectangle)sourceRectangle;
+            }
+            set { sourceRectangle = value; }
+        }
+
+        public Color Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+
+        public float Rotation
+        {
+            get { return rotation; }
+            set { rotation = value; }
+        }
+
+        public Vector2 RotationOrigin
+        {
+            get { return origin; }
+            set { origin = value; }
+        }
+
+        public Vector2 Scale
+        {
+            get { return scale; }
+            set { scale = value; }
+        }
+
+        public SpriteEffects Effect
+        {
+            get { return effect; }
+            set { effect = value; }
+        }
+
+        public float LayerDepth
+        {
+            get { return layerDepth; }
+            set { layerDepth = value; }
+        }
+
+        public bool ColisionRectangleIsVisible
+        {
+            get { return colisionRectangleVisible; }
+            set { colisionRectangleVisible = value; }
+        }
+
+        public bool Colision
+        {
+            get { return colision; }
+            set { colision = value; }
+        }
+
+        public Sprite(Vector2 position)
+        {
+            this.position = position;
+        }
+
+        public Sprite(float x, float y)
+        {
+            position = new Vector2(x, y);
+        }
+
+        public Sprite(Rectangle destinationRectangle)
+        {
+            this.destinationRectangle = destinationRectangle;
+        }
+
+        public Sprite(Rectangle destinationRectangle, Rectangle? sourceRectangle)
+        {
+            this.destinationRectangle = destinationRectangle;
+            this.SourceRectangle = sourceRectangle;
+        }
+
+        public Sprite(Vector2 position, Rectangle? sourceRectangle)
+        {
+            this.position = position;
+            this.sourceRectangle = sourceRectangle;
+        }
+
+        public Sprite(float x, float y, Rectangle? sourceRectangle)
+        {
+            position = new Vector2(x, y);
+            this.sourceRectangle = sourceRectangle;
+        }
+
+        public Sprite(Vector2 position, Rectangle? sourceRectangle, float rotation, Vector2 origin, Vector2 scale)
+        {
+            this.position = position;
+            this.sourceRectangle = sourceRectangle;
+            this.rotation = rotation;
+            this.origin = origin;
+            this.scale = scale;
+        }
+
+        public void LoadContent(TextureManager textureManager, string assetName)
+        {
+            texture = textureManager.GetTexture(assetName);
+        }
+
+        public Texture2D Texture
+        {
+            get { return texture; }
+        }
+
+        public Rectangle ColisionRectangle
+        {
+            get
+            {
+                if (colisionRectangle == null)
+                {
+                    this.colisionRectangle = new Rectangle((int)position.X, (int)position.Y, SourceRectangle.Value.Width, SourceRectangle.Value.Height);
+                    return (Rectangle)colisionRectangle;
+                }
+                else
+                    return (Rectangle)colisionRectangle;
+            }
+            set { colisionRectangle = value; }
+        }
+
+        public Rectangle ColisionRectangle2
+        {
+            get { return new Rectangle((int)position.X, (int)position.Y, SourceRectangle.Value.Width, SourceRectangle.Value.Height); }
+            set { colisionRectangle = value; }
+        }
+
+        public void Update(Vector2 translation)
+        {
+            position += translation;
+            if (colision)
+            {
+                colisionRectangle = new Rectangle((int)position.X, (int)position.Y, sourceRectangle.Value.Width, sourceRectangle.Value.Height);
+            }
+        }
+
+        public void LoadContent(TextureManager textureManager, string assetName, int maxIndexX, int maxIndexY, string readingDimension)
+        {
+            if (colisionRectangleVisible)
+            {
+                textureColisionRectangle = textureManager.GetTexture("rectangle");
+            }
+            texture = textureManager.GetTexture(assetName);
+            this.maxIndexX = maxIndexX;
+            this.maxIndexY = maxIndexY;
+            this.readingDimension = readingDimension;
+        }
+
+        public void UpdateAnimation(GameTime gameTime)
+        {
+            if (maxIndexX != 0)
+            {
+                indexX += gameTime.ElapsedGameTime.Milliseconds * 0.007f;
+
+                if (indexX > maxIndexX)
+                {
+                    indexX = 0;
+                }
+                if (readingDimension == "h")
+                {
+                    sourceRectangle = new Rectangle((int)indexX * sourceRectangle.Value.Width, sourceRectangle.Value.Y, sourceRectangle.Value.Width, sourceRectangle.Value.Height);
+                }
+                if (readingDimension == "v")
+                {
+                    sourceRectangle = new Rectangle(sourceRectangle.Value.X, (int)indexY * sourceRectangle.Value.Height, sourceRectangle.Value.Width, sourceRectangle.Value.Height);
+                }
+            }
+        }
+
+        public void UpdateSetStateAnimation(int index)
+        {
+            if (readingDimension == "h")
+            {
+                sourceRectangle = new Rectangle(index * sourceRectangle.Value.Width, sourceRectangle.Value.Y, sourceRectangle.Value.Width, sourceRectangle.Value.Height);
+            }
+            if (readingDimension == "v")
+            {
+                sourceRectangle = new Rectangle(sourceRectangle.Value.X, index * sourceRectangle.Value.Height, sourceRectangle.Value.Width, sourceRectangle.Value.Height);
+            }
+        }
+
+        public void UpdateSetStateAnimation(int indexX, int indexY)
+        {
+            sourceRectangle = new Rectangle(indexX * sourceRectangle.Value.Width, indexY * sourceRectangle.Value.Height, sourceRectangle.Value.Width, sourceRectangle.Value.Height);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, position, sourceRectangle, Color.White, rotation, origin, scale, effect, layerDepth);
+            if (colisionRectangleVisible)
+            {
+                spriteBatch.Draw(textureColisionRectangle,(Rectangle)colisionRectangle, Color.White);
+            }
+        }
+    }
+}
