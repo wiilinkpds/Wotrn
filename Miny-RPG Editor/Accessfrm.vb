7f02dc91f100a0323004239b2ef1bede3757ef90
Psyker <nikon-1@hotmail.fr>
2013-01-23 22:05:25 +0100

    Editor fonctionnel

--- Miny-RPG Editor/Accessfrm.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class Accessfrm
-
-    Private Sub Accessfrm_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-
-        Me.Location = Mainfrm.Location + New Point(30, 20)
-
-    End Sub
-
-End Class
